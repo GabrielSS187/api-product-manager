c73038b9444d598fe045ad32bf88a2b51f9bdd57
Gabriel Silva <gabriel127.gk@gmail.com>
2023-04-07 20:37:59 -0300

    Criando arq. auth-middleware

---
+++ src/middlewares/auth-middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+
+import { JwtAdapter } from "../adapters/JwtAdapter/Jwt-adapter";
+
+const jwt = new JwtAdapter();
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) throw new Error("Unauthorized, token is required");
+
+  const token = authorization.replace("Bearer", "").trim();
+
+  try {
+    const decoded = jwt.getToken({ token });
+
+    const { role } = decoded;
+    req.userRole = role;
+
+    next();
+    // rome-ignore lint/suspicious/noExplicitAny: <explanation>
+  } catch (error: any) {
+    return res.status(401).json(error.message);
+  }
+};
