d957942491ae06f321b24ff12f7f63abe6aea7c5
Gabriel Silva <gabriel127.gk@gmail.com>
2023-04-07 20:27:57 -0300

    Ageitando o arq. database/database-connection

--- src/database/Database-connection.ts
+++ src/database/Database-connection.ts
@@ -1,6 +1,8 @@
 import { env } from "process";
+import { config } from "dotenv";
 import mongoose from "mongoose";
 
+config();
 // rome-ignore lint/style/noNonNullAssertion: <explanation>
 mongoose.connect(env.DATABASE_URL!)
 .then(() => {
