0d5c8da9dd5acd60b51a3fab4dfee468c4288d55
Gabriel Silva <gabriel127.gk@gmail.com>
2023-04-08 23:53:00 -0300

    Formatando documento.

--- src/tests/product.spec.ts
+++ src/tests/product.spec.ts
@@ -57,7 +57,7 @@ describe("Create-product-case", () => {
 			const result = await sutCreateProduct.create({
 				role: "normal",
 				body: newProduct,
-			});      
+			});
 			expect(result.statusCode).not.toBe(201);
 			expect(result.success).toBeUndefined();
 			// rome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -67,64 +67,59 @@ describe("Create-product-case", () => {
 			expect(error.message).toBe(
 				"Not authorized. You are not an administrator.",
 			);
-		};
+		}
 	});
 
-  test("Should throw an error if the product name already exists in the list.", async () => {
-    const { sutCreateProduct } = sutFactory();
+	test("Should throw an error if the product name already exists in the list.", async () => {
+		const { sutCreateProduct } = sutFactory();
 
-    try {
+		try {
 			const result = await sutCreateProduct.create({
 				role: "admin",
-				body: {...newProduct},
-			});     
+				body: { ...newProduct },
+			});
 			expect(result.statusCode).not.toBe(201);
 			expect(result.success).toBeUndefined();
 			// rome-ignore lint/suspicious/noExplicitAny: <explanation>
 		} catch (error: any) {
 			expect(error).toBeInstanceOf(ProductError);
 			expect(error.statusCode).toBe(409);
-			expect(error.message).toBe(
-				"A product with that name already exists.",
-			);
-		};
-  });
+			expect(error.message).toBe("A product with that name already exists.");
+		}
+	});
 
-  test("It should throw an error if the user chooses more than 2 categories.", async () => {
-    const { sutCreateProduct } = sutFactory();
-   
-    newProduct.name = "Jatot";
-    newProduct.categories = ["pc", "console", "monitor", "game"];
+	test("It should throw an error if the user chooses more than 2 categories.", async () => {
+		const { sutCreateProduct } = sutFactory();
 
-    try {
+		newProduct.name = "Jatot";
+		newProduct.categories = ["pc", "console", "monitor", "game"];
+
+		try {
 			const result = await sutCreateProduct.create({
 				role: "admin",
 				body: newProduct,
-			});     
+			});
 			expect(result.statusCode).not.toBe(201);
 			expect(result.success).toBeUndefined();
 			// rome-ignore lint/suspicious/noExplicitAny: <explanation>
 		} catch (error: any) {
 			expect(error).toBeInstanceOf(ProductError);
 			expect(error.statusCode).toBe(406);
-			expect(error.message).toBe(
-				"You cannot choose more than 2 categories.",
-			);
-		};    
-  });
+			expect(error.message).toBe("You cannot choose more than 2 categories.");
+		}
+	});
 
-  test("It should throw an error if the chosen category is different from those allowed.",
-  async () => {
-    const { sutCreateProduct } = sutFactory();
+	test("It should throw an error if the chosen category is different from those allowed.", async () => {
+		const { sutCreateProduct } = sutFactory();
 
-    newProduct.name = "GTA 5";
-    newProduct.categories = ["carro"];
+		newProduct.name = "GTA 5";
+		newProduct.categories = ["carro"];
 
-    try {
+		try {
 			const result = await sutCreateProduct.create({
 				role: "admin",
 				body: newProduct,
-			});     
+			});
 			expect(result.statusCode).not.toBe(201);
 			expect(result.success).toBeUndefined();
 			// rome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -134,44 +129,42 @@ describe("Create-product-case", () => {
 			expect(error.message).toBe(
 				"The only allowed categories are: pc,console,monitor,game.",
 			);
-		};    
-  });
+		}
+	});
 
-  test("Should throw an error if no category is chosen.",async () => {
-    const { sutCreateProduct } = sutFactory();
+	test("Should throw an error if no category is chosen.", async () => {
+		const { sutCreateProduct } = sutFactory();
 
-    newProduct.name = "Avião";
-    newProduct.categories = [];
+		newProduct.name = "Avião";
+		newProduct.categories = [];
 
-    try {
+		try {
 			const result = await sutCreateProduct.create({
 				role: "admin",
 				body: newProduct,
-			});     
+			});
 			expect(result.statusCode).not.toBe(201);
 			expect(result.success).toBeUndefined();
 			// rome-ignore lint/suspicious/noExplicitAny: <explanation>
 		} catch (error: any) {
 			expect(error).toBeInstanceOf(ProductError);
 			expect(error.statusCode).toBe(406);
-			expect(error.message).toBe(
-				"Must have at least one category.",
-			);
-		}; 
-  });
+			expect(error.message).toBe("Must have at least one category.");
+		}
+	});
 
-  test("Should throw an error if the quantity is less than 1.", async () => {
-    const { sutCreateProduct } = sutFactory();
+	test("Should throw an error if the quantity is less than 1.", async () => {
+		const { sutCreateProduct } = sutFactory();
 
-    newProduct.name = "Nintendo";
-    newProduct.categories = ["game"];
-    newProduct.qty = 0;
+		newProduct.name = "Nintendo";
+		newProduct.categories = ["game"];
+		newProduct.qty = 0;
 
-    try {
+		try {
 			const result = await sutCreateProduct.create({
 				role: "admin",
 				body: newProduct,
-			});     
+			});
 			expect(result.statusCode).not.toBe(201);
 			expect(result.success).toBeUndefined();
 			// rome-ignore lint/suspicious/noExplicitAny: <explanation>
@@ -179,29 +172,29 @@ describe("Create-product-case", () => {
 			expect(error).toBeInstanceOf(ProductError);
 			expect(error.statusCode).toBe(406);
 			expect(error.message.length).toBeGreaterThan(0);
-		};
-  });
+		}
+	});
 
-  test("Should throw an error if the price is less than 5.", async () => {
-    const { sutCreateProduct } = sutFactory();
+	test("Should throw an error if the price is less than 5.", async () => {
+		const { sutCreateProduct } = sutFactory();
 
-    newProduct.name = "Nintendo";
-    newProduct.categories = ["game"];
-    newProduct.qty = 10;
-    newProduct.price = 2.99;
+		newProduct.name = "Nintendo";
+		newProduct.categories = ["game"];
+		newProduct.qty = 10;
+		newProduct.price = 2.99;
 
-    try {
+		try {
 			const result = await sutCreateProduct.create({
 				role: "admin",
 				body: newProduct,
-			});         
+			});
 			expect(result.statusCode).not.toBe(201);
 			expect(result.success).toBeUndefined();
 			// rome-ignore lint/suspicious/noExplicitAny: <explanation>
-		} catch (error: any) {      
+		} catch (error: any) {
 			expect(error).toBeInstanceOf(ProductError);
 			expect(error.statusCode).toBe(406);
 			expect(error.message.length).toBeGreaterThan(0);
-		};
-  });
+		}
+	});
 });
